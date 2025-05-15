1cde063053f7644bddb0cf1152b54f165c2d4f2f
karpov2057 <karpov2057@yandex.ru>
2025-05-15 08:50:43 +0500

    первый коммит

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import style from './index.module.scss';
+import './index.scss';
+import './assets/images/file.jpg';
+import { ReactComponent as ReactLogo } from './assets/images/react.svg';
+
+console.log(style);
+const numbers:number[] = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
